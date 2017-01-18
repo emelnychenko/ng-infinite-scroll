1b60d669a9aa86891e34b825a86e1cb903f87f5e
Eugen Melnychenko <eugen.melnychenko@gmail.com>
2017-01-18 12:19:54 +0200

    Update angular-infinite-scroll.js

--- angular-infinite-scroll.js
+++ angular-infinite-scroll.js
@@ -1,16 +1,16 @@
 angular.module('fx.infinite.scroll', [])
     .factory('fxScrollFactory', ['$window', function($window) {
         var $scroll = {
-            "offset"    : 0,
+            "offset"    : undefined,
             "calculate" : function(element, distantion) {
-                $scoll.offset = parseInt(
-                    $window.getComputedStyle(
+                $scroll.offset = parseInt(
+                    window.getComputedStyle(
                         element[0], null
                     ).getPropertyValue("height").replace(/px/, '')
-                ) - $window.innerHeight - (
+                ) - window.innerHeight - (
                     distantion ? distantion : 50
                 );
-            }
+            },
             "group"     : {
                 "current"    : 'default',
                 "dependence" : new Object,
@@ -23,20 +23,23 @@ angular.module('fx.infinite.scroll', [])
             "renew"     : function(name) {
                 if ($scroll.chain[name])
                     $scroll.chain[name] = false;
+
+                $scroll.calculate();
             },
             "switch"     : function(group) {
                 $scroll.group.current = group;
+                $scroll.calculate();
             },
         };
 
         return $scroll;
     }])
-    .directive('fxScroll', ['fxScrollFactory', function($scroll) {
+    .directive('fxScroll', ['fxScrollFactory', '$parse', function($scroll, $parse) {
         return {
             restrict: 'A',
             link: function($scope, $element, $attr) {
-                var height      = 0, 
-                    name        = $attr.fxScroll, 
+                var height      = 0,
+                    name        = $attr.fxScroll,
                     group       = $attr.fxScrollGroup,
                     reverse     = $attr.fxScrollReverse ? true : false,
                     winoff      = false,
@@ -48,31 +51,33 @@ angular.module('fx.infinite.scroll', [])
 
                 $scroll.append(name, group);
 
-                (
+                angular.element(
                     $attr.fxScrollRoot ? document.querySelector(
                         $attr.fxScrollRoot
-                    ) : $window
-                ).onscroll = function() {
-                    if (!$scoll.height) $scoll.calculate(
-                        $element, $attr.fxScrollDist 
+                    ) : window
+                ).bind("scroll", function() {
+                    if (!$scroll.offset) $scroll.calculate(
+                        $element, $attr.fxScrollDist
                     );
 
                     winoff = this.pageYOffset || this.scrollTop;
 
-                    if (reverse ? ($scoll.height > winoff) : ($scoll.height < winoff)) {
-                        if (!$scope.chain[name] && $scope.group.current === group) {
-                            $scope.chain[name] = true;
+                    if (reverse ? ($scroll.offset > winoff) : ($scroll.offset < winoff)) {
+                        if (!$scroll.chain[name] && $scroll.group.current === group) {
+                            $scroll.chain[name] = true;
 
-                            handler ? handler(function() {
-                                $scope.chain[name] = false;
-                                
-                                $scoll.calculate(
-                                    $element, $attr.fxScroll
-                                );
-                            }) : null;
+                            if (handler) {
+                                handler(function() {
+                                    $scroll.chain[name] = false;
+
+                                    $scroll.calculate(
+                                        $element, $attr.fxScroll
+                                    );
+                                });
+                            }
                         }
                     }
-                };
+                });
             }
         };
     }]);
